5f368400a5a9611013c54d84a210bb6655336863
Andrii Tochylov <andretochilov1@gmail.com>
2020-04-19 22:19:12 +0300

    new prog

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,25 @@
 package domain;
-
+/**
+ * Solves the given problem (Finds evil numbers in given range)
+ * @author Andrei
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+    private static boolean checkEvilNumber(String binary) {
+        short count = 0;
+        for(short i = 0; i<binary.length(); i++){
+            if(binary.charAt(i) == '1') count++;
+        }
+        return (count % 2) == 0;
+    }
+
+    public static String Calculate(short start, short end) {
+        String res = "";
+        for(short i = start; i<=end; i++){
+            String binary = Integer.toBinaryString(i);
+            if(checkEvilNumber(binary))
+                res +=  Integer.toString(i)+ ", ";
+        }
+        return res.substring(0, res.length()-2) + ";";
     }
 }
